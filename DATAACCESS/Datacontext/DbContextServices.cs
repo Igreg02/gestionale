0d98ebcc81eef9ddea9dc16e1b6f6dbe72af5e2a
Igreg02 <I.gregale@gmail.com>
2026-07-03 17:08:31 +0200

    clean code X2

--- DATAACCESS/Datacontext/DbContextServices.cs
+++ DATAACCESS/Datacontext/DbContextServices.cs
@@ -9,24 +9,16 @@ namespace GestionaleRendicontazione.Dataaccess.Datacontext.DbContextService
     {
         private readonly IDataLayer _dataLayer;
 
-        // Il DataLayer viene iniettato ed è unico per l'applicazione (Singleton)
         public DbContextService(IDataLayer dataLayer)
         {
             _dataLayer = dataLayer;
         }
 
-
-
-
-
-
-
         public T ExecuteReadOnly<T>(Func<Session, T> query)
         {
             using (var session = new Session(_dataLayer))
             {
-                // Disabilita il tracciamento delle modifiche perchè verra utilizzato come ReadOnly
-                session.TrackPropertiesModifications = false;
+                session.TrackPropertiesModifications = false; // Risparmio risorse è in readonly
 
                 return query(session);
             }
@@ -36,7 +28,6 @@ namespace GestionaleRendicontazione.Dataaccess.Datacontext.DbContextService
         {
             using (var session = new Session(_dataLayer))
             {
-                // Disabilita il tracciamento delle modifiche perchè verra utilizzato come ReadOnly
                 session.TrackPropertiesModifications = false;
 
                 query(session);
@@ -50,14 +41,11 @@ namespace GestionaleRendicontazione.Dataaccess.Datacontext.DbContextService
             {
                 try
                 {
-                    // Esegue l'operazione passata tramite Lambda (es: inserimento, update)
                     await operation(uow);
-                    // Salva le modifiche nel database in modo asincrono
                     await uow.CommitChangesAsync();
                 }
                 catch (Exception)
                 {
-                    // futuro log errore
                     throw;
                 }
             }
